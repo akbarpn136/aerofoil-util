c2cd8cdb2d6b475324ebab6a14c37db703e8edb6
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-13 12:20:10 +0700

    Applying multiprocessing on generating image

--- src/services/image.py
+++ src/services/image.py
@@ -1,10 +1,11 @@
 import os
-
 import cv2
-import numpy as np
 import skfmm
 import typer
+import numpy as np
+from functools import partial
 from matplotlib import pyplot as plt
+from multiprocessing import Pool, cpu_count
 
 
 def rotating(draw, angle):
@@ -21,52 +22,64 @@ def rotating(draw, angle):
     return rotated
 
 
-def rendering(name, points, resolution, kind, start, stop, re, ma):
+def to_img(angle, path, points, resolution, kind, re, ma):
     # Image has 2 dimensions shape
     dimension = 2
+    offset_y = resolution // 2
+    phi = -1 * np.ones((resolution, resolution, 1), dtype="uint8")
+    airfoils = np.empty((0, dimension), int)
 
-    for angle in range(start, stop + 1):
-        offset_y = resolution // 2
-        phi = -1 * np.ones((resolution, resolution, 1), dtype="uint8")
-        airfoils = np.empty((0, dimension), int)
+    for point in points:
+        airfoils = np.append(
+            airfoils,
+            np.array([[
+                round(resolution * point["x"]),
+                round(resolution * point["y"] + offset_y)
+            ]]),
+            axis=0
+        )
 
-        for point in points:
-            airfoils = np.append(
-                airfoils,
-                np.array([[
-                    round(resolution * point["x"]),
-                    round(resolution * point["y"] + offset_y)
-                ]]),
-                axis=0
-            )
+    airfoils = airfoils.reshape((-1, 1, 2))
+    cv2.fillPoly(phi, [airfoils], (255, 255, 255), lineType=cv2.LINE_AA)
+    phi = cv2.flip(phi, 0)
+    phi = rotating(phi, angle)
 
-        airfoils = airfoils.reshape((-1, 1, 2))
-        cv2.fillPoly(phi, [airfoils], (255, 255, 255), lineType=cv2.LINE_AA)
-        phi = cv2.flip(phi, 0)
-        phi = rotating(phi, angle)
+    fig, ax = plt.subplots()
+    plt.margins(x=0, y=0)
+    plt.axis("off")
+    ax.set_box_aspect(1)
+    plt.tight_layout()
 
-        fig, ax = plt.subplots()
-        plt.margins(x=0, y=0)
-        plt.axis("off")
-        ax.set_box_aspect(1)
-        plt.tight_layout()
+    if kind == "binary":
+        colormap = "gray"
+    elif kind == "sdf":
+        colormap = "plasma"
+        phi = skfmm.distance(phi, dx=1, order=2)
+    else:
+        colormap = "gray"
 
-        if kind == "binary":
-            colormap = "gray"
-        elif kind == "sdf":
-            colormap = "plasma"
-            phi = skfmm.distance(phi, dx=1, order=2)
-        else:
-            colormap = "gray"
+    plt.imshow(phi, cmap=plt.get_cmap(colormap))
 
-        plt.imshow(phi, cmap=plt.get_cmap(colormap))
+    plt.savefig(f"{path}/{kind}_{re}_{ma}_{angle}.jpg", bbox_inches="tight", pad_inches=0)
+    plt.close("all")
 
-        path = f"out/{name}"
-        isExist = os.path.exists(path)
 
-        if not isExist:
-            typer.secho("Creating new folder", fg=typer.colors.YELLOW)
-            os.makedirs(path)
+def rendering(name, points, resolution, kind, start, stop, re, ma):
+    path = f"out/{name}"
+    isExist = os.path.exists(path)
 
-        plt.savefig(f"{path}/{kind}_{re}_{ma}_{angle}.jpg", bbox_inches="tight", pad_inches=0)
-        plt.close("all")
+    if not isExist:
+        typer.secho("Creating new folder", fg=typer.colors.YELLOW)
+        os.makedirs(path)
+
+    with Pool(cpu_count()) as pool:
+        partial_func = partial(
+            to_img,
+            path=path,
+            points=points,
+            resolution=resolution,
+            kind=kind,
+            re=re,
+            ma=ma
+        )
+        pool.map(partial_func, range(start, stop + 1))
