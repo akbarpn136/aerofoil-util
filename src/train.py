73824240e5b27be395553eb505c62d4c75dc765e
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-14 09:23:12 +0700

    Normalize image data

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ from services.collection import AerofoilForceDataset
 
 if __name__ == '__main__':
     batch_size = 21
-    num_epochs = 30
+    num_epochs = 100
     learning_rate = 0.00001
 
     dataset = AerofoilForceDataset(
@@ -18,9 +18,9 @@ if __name__ == '__main__':
         transform=transforms.Compose([
             transforms.Resize(128),
             transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
     )
-
     dataset_size = len(dataset)
     train_size = int(0.8 * dataset_size)
     valid_size = dataset_size - train_size
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     loss_func = nn.MSELoss()
     optim = torch.optim.Adam(model.parameters(), learning_rate)
 
-    model.fit(loss_func, optim, train_loader, valid_loader, epochs=100)
+    model.fit(loss_func, optim, train_loader, valid_loader, epochs=num_epochs)
 
     torch.save(model.state_dict(), "aerocnn.pt")
 
