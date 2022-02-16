1cb028197e372391230965f24f4c7d561458a142
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-16 17:16:59 +0700

    Modify function for prediction

--- src/controllers/pred.py
+++ src/controllers/pred.py
@@ -22,51 +22,43 @@ def predict(
     Function to predict the airfoil coeficient aerodynamic
     (Cl, Cd and Cm) with varying angle of attack.
     """
-    images = []
-    angles = []
     path = f"out/{airfoilname}"
     all_files = glob.glob(f"{path}/*.jpg")
+    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = AerofoilNN().to(dev)
+    model.load_state_dict(
+        torch.load("aerocnn.pt", map_location=dev)
+    )
+    model.eval()
 
+    arr = np.empty((0, 4), int)
     for filename in all_files:
         split = filename.split("_")[-1]
         angle = split.replace(".jpg", "")
         angle = int(angle)
-        angles.append(angle)
         img = Image.open(filename)
-        img = np.asarray(img)
 
-        images.append(img)
+        transform = transforms.Compose([
+            transforms.Resize(128),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
-    images = np.array(images, dtype=np.float32)
-    images = images.reshape((
-        images.shape[0],
-        3,
-        images.shape[1],
-        images.shape[2]
-    ))
-    images = torch.from_numpy(images)
-
-    transform = transforms.Compose([
-        transforms.Resize(128),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    img = transform(images)
-
-    with torch.no_grad():
-        dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model = AerofoilNN(None).to(dev)
-        model.load_state_dict(
-            torch.load("aerocnn.pt", map_location=dev)
+        img = transform(img).unsqueeze(0)
+        pred = model(img.to(dev))
+        pred = pred.detach().numpy() * 0.5 + 0.5  # Denormalize
+        arr = np.append(
+            arr,
+            np.array([
+                [angle, pred[0, 0], pred[0, 1], pred[0, 2]]
+            ]),
+            axis=0
         )
 
-        res = model(img.to(dev))
-        df = pd.DataFrame(res)
-        df.columns = ["cl", "cd", "cm"]
-        df = (df * 0.5) + 0.5
-        df["sudut"] = angles
-        df = df.sort_values("sudut")
+    df = pd.DataFrame(arr)
+    df.columns = ["alpha", "cl", "cd", "cm"]
+    df["name"] = airfoilname
+    df = df.sort_values("alpha")
 
-        df.to_csv("prediction.csv")
-
-        typer.secho(f"Saving prediction result", fg=typer.colors.MAGENTA)
+    typer.secho(f"Saving prediction result", fg=typer.colors.MAGENTA)
+    df.to_csv("prediction.csv", index=False)
