4bf5fccd23681f4cf31d34d50abea424ad98b0ab
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-18 21:12:46 +0700

    Add new command to get aerodynamic airfoil data

---
+++ src/controllers/get.py
@@ -0,0 +1,64 @@
+import glob
+import time
+import typer
+import pandas as pd
+
+from . import app
+
+
+@app.command()
+def get(
+        path: str = typer.Option(
+            "aero",
+            help="Path where aerodyanmic data is saved from XFOIL.",
+        ),
+        kind: str = typer.Option(
+            "sdf",
+            "--kind",
+            "-k",
+            help="Airfoil geometry representation: binary, mesh or sdf.",
+        ),
+):
+    """
+    Function to get data airfoil aerodynamic
+    extracted from XFOIL.
+    """
+    start = time.time()
+    payload = []
+    all_files = glob.glob(f"{path}/*.txt")
+    for filename in all_files:
+        df = _process(path, filename, kind)
+        payload.append(df)
+
+    dt = pd.concat(payload, axis=0)
+    dt.to_csv("out.csv", index=False)
+    typer.secho(f"Successfully save data. Took {time.time() - start} s", fg=typer.colors.GREEN)
+
+
+def _process(path, filename, kind):
+    file = filename.replace(path, "") \
+        .replace("\\", "") \
+        .replace("/", "")
+
+    split = file.split("_")
+    name = split[0].replace(" ", "")
+    re = split[2].replace("Re", "")
+    re = int(float(re) * 10 ** 6)
+    ma = split[3].replace("M", "")
+    ma = float(ma)
+    image = f"{name}_{kind}_{re}_{ma}"
+
+    df = pd.read_fwf(
+        filename,
+        skiprows=11,
+        header=None,
+        names=["alpha", "cl", "cd", "cdp", "cm", "TopXtr", "BotXtr", "Cpmin", "Chinge", "XCp"]
+    )
+
+    df = df[["alpha", "cl", "cd", "cm"]]
+    df["name"] = name
+    df["re"] = re
+    df["ma"] = ma
+    df["img"] = df.apply(lambda row: f"{image}_{int(row['alpha'])}.jpg", axis=1)
+
+    return df
