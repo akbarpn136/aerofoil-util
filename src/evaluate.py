38cd151ef1cd8cecef2132470f24f03a39da3ded
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-20 10:50:04 +0700

    Modify evaluating function

--- src/evaluate.py
+++ src/evaluate.py
@@ -39,21 +39,25 @@ def rsquare(y, yhat):
 
 
 if __name__ == "__main__":
-    dt = pd.read_csv("../out.csv")
-    dt = dt.loc[dt["name"] == "NACA0012"]
-    dt = dt[["alpha", "cl", "cd", "cm"]]
-    dt = dt.sort_values("alpha")
-    df = pd.read_csv("../prediction.csv")
+    pred = pd.read_csv("../prediction.csv")
+    pred_nama = pred["name"].unique()[0]
+
+    aktual = pd.read_csv("../out.csv")
+    aktual = aktual.loc[aktual["name"] == pred_nama]
+    aktual = aktual[["alpha", "cl", "cd", "cm"]]
+    aktual = aktual.sort_values("alpha")
 
     # plt.style.use("bmh")
-    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(11, 9))
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(10, 8))
     fig.tight_layout(pad=4.0)
+
+    plt.suptitle(pred_nama, fontsize=14)
 
     plotting(
         ax1,
         "Cl - Alpha",
-        dt["alpha"].to_numpy(), dt["cl"].to_numpy(),
-        df["alpha"].to_numpy(), df["cl"].to_numpy(),
+        aktual["alpha"].to_numpy(), aktual["cl"].to_numpy(),
+        pred["alpha"].to_numpy(), pred["cl"].to_numpy(),
         "Actual", "Prediction",
         "alpha", "Cl", "x"
     )
@@ -61,8 +65,8 @@ if __name__ == "__main__":
     plotting(
         ax2,
         "Cd - Alpha",
-        dt["alpha"].to_numpy(), dt["cd"].to_numpy(),
-        df["alpha"].to_numpy(), df["cd"].to_numpy(),
+        aktual["alpha"].to_numpy(), aktual["cd"].to_numpy(),
+        pred["alpha"].to_numpy(), pred["cd"].to_numpy(),
         "Actual", "Prediction",
         "alpha", "Cd", "x"
     )
@@ -70,14 +74,18 @@ if __name__ == "__main__":
     plotting(
         ax3,
         "Cm - Alpha",
-        dt["alpha"].to_numpy(), dt["cm"].to_numpy(),
-        df["alpha"].to_numpy(), df["cm"].to_numpy(),
+        aktual["alpha"].to_numpy(), aktual["cm"].to_numpy(),
+        pred["alpha"].to_numpy(), pred["cm"].to_numpy(),
         "Actual", "Prediction",
         "alpha", "Cm", "x"
     )
 
-    x4 = dt["cl"].to_numpy()
-    y4 = df["cl"].to_numpy()
+    x4 = aktual["cl"].to_numpy()
+    y4 = pred["cl"].to_numpy()
+
+    if y4.size > x4.size:
+        y4 = y4[:(x4.size - y4.size)]
+
     coef4 = np.polyfit(x4, y4, 1)
     poly4 = np.poly1d(coef4)
     rsq5 = rsquare(x4, y4)
@@ -90,8 +98,12 @@ if __name__ == "__main__":
         "Actual Cl", "Predicted Cl", "o"
     )
 
-    x5 = dt["cd"].to_numpy()
-    y5 = df["cd"].to_numpy()
+    x5 = aktual["cd"].to_numpy()
+    y5 = pred["cd"].to_numpy()
+
+    if y5.size > x5.size:
+        y5 = y5[:(x5.size - y5.size)]
+
     coef5 = np.polyfit(x5, y5, 1)
     poly5 = np.poly1d(coef5)
     rsq5 = rsquare(x5, y5)
@@ -104,8 +116,12 @@ if __name__ == "__main__":
         "Actual Cd", "Predicted Cd", "o"
     )
 
-    x6 = dt["cm"].to_numpy()
-    y6 = df["cm"].to_numpy()
+    x6 = aktual["cm"].to_numpy()
+    y6 = pred["cm"].to_numpy()
+
+    if y6.size > x6.size:
+        y6 = y6[:(x6.size - y6.size)]
+
     coef6 = np.polyfit(x6, y6, 1)
     poly6 = np.poly1d(coef6)
     rsq6 = rsquare(x6, y6)
