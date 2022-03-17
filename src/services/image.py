09582f74796fc6f7a7a1cec281404872cc9d839c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-03-17 22:25:08 +0700

    Correcting the colormap

--- src/services/image.py
+++ src/services/image.py
@@ -1,5 +1,6 @@
 import cv2
 import skfmm
+import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -42,12 +43,15 @@ def rendering(name, angle, points, resolution, kind, re, ma):
     phi = cv2.copyMakeBorder(phi, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=[-1, -1, -1])
     phi = rotating(phi, angle)
 
+    colormap = "gray"
     if kind == "sdf":
+        colormap = "jet"
         phi = skfmm.distance(phi, dx=1, order=2)
 
+    matplotlib.use("Agg")
     fig, ax = plt.subplots()
     fig.tight_layout()
-    ax.imshow(phi, cmap=plt.get_cmap("jet"))
-    ax.axes.axis('off')
-    fig.savefig(f"../out/{airfoil_image_name.replace(' ', '')}.jpg", bbox_inches="tight", pad_inches=0, dpi=34.7)
+    ax.imshow(phi, cmap=plt.get_cmap(colormap))
+    ax.axes.axis("off")
+    fig.savefig(f"../out/{airfoil_image_name.replace(' ', '')}.jpg", bbox_inches="tight", pad_inches=0)
     plt.close(fig)
