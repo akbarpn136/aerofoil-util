29c05e5ff9b9c5a4ecab91c3273ba29660080c91
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-03-29 18:43:51 +0700

    Modify read image for 1 or 3 channels

--- src/services/collection.py
+++ src/services/collection.py
@@ -5,17 +5,22 @@ from torch.utils.data import Dataset
 
 
 class AerofoilForceDataset(Dataset):
-    def __init__(self, csvfile, imgdir, transform=None):
+    def __init__(self, csvfile, imgdir, transform=None, num_channel=3):
         self.df = pd.read_csv(csvfile)
         self.imgdir = imgdir
         self.transform = transform
+        self.num_channel = num_channel
 
     def __len__(self):
         return self.df.index.size
 
     def __getitem__(self, item):
         img = Image.open(f"{self.imgdir}/{self.df.iloc[item]['img']}")
-        img = img.convert("RGB")
+
+        if self.num_channel == 3:
+            img = img.convert("RGB")
+        else:
+            img = img.convert("L")
 
         labels = self.df.iloc[item][["cl", "cd", "cm"]].to_numpy(dtype="float32")
         labels = denorm(labels, 0.5, 0.5)
