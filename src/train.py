92f04608ac7522a7a926428da8dee86e9c74bf3b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-03-29 18:44:46 +0700

    Train CNN model for images with 1 or 3 channels

--- src/train.py
+++ src/train.py
@@ -9,7 +9,8 @@ from services.collection import AerofoilForceDataset
 
 if __name__ == '__main__':
     batch_size = 41
-    num_epochs = 500
+    num_channel = 1
+    num_epochs = 1000
     learning_rate = 0.00001
 
     dataset = AerofoilForceDataset(
@@ -18,8 +19,10 @@ if __name__ == '__main__':
         transform=transforms.Compose([
             transforms.Resize(128),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) if num_channel == 3
+            else transforms.Normalize((0.5,), (0.5,)),
+        ]),
+        num_channel=num_channel
     )
     dataset_size = len(dataset)
     train_size = int(0.8 * dataset_size)
@@ -29,13 +32,13 @@ if __name__ == '__main__':
     valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=True)
 
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = AerofoilNN().to(dev)
+    model = AerofoilNN(num_channel=num_channel).to(dev)
     loss_func = nn.MSELoss()
     optim = torch.optim.Adam(model.parameters(), learning_rate)
 
     model.fit(loss_func, optim, train_loader, valid_loader, device=dev, epochs=num_epochs)
 
-    torch.save(model.state_dict(), "aerocnn.pt")
+    torch.save(model.state_dict(), "aerofoil_mesh_4Conv_BN_2FC.pt")
 
     plt.plot(model.lossList[1:], label="Train Loss")
     plt.plot(model.valid_lossList[1:], label="Valid Loss")
