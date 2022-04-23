ab17d61939fd659ac0bcc291fc4a605ee79da90e
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-23 15:13:04 +0700

    Create new function image generation

--- src/services/image.py
+++ src/services/image.py
@@ -1,14 +1,19 @@
+import os
 import cv2
 import skfmm
 import matplotlib
 import numpy as np
 from PIL import Image
+from scipy.io import wavfile
 from matplotlib import pyplot as plt
+
+from src.services.spectro import SpectroGraphic
 
 
 def rotating(draw, angle):
     center = (draw.shape[0] // 2, draw.shape[1] // 2)
-    rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=-angle, scale=1)
+    rotate_matrix = cv2.getRotationMatrix2D(
+        center=center, angle=-angle, scale=1)
     rotated = cv2.warpAffine(
         src=draw,
         M=rotate_matrix,
@@ -44,7 +49,8 @@ def rendering_sdf(name, angle, points, resolution, kind, re, ma):
     airfoils = airfoils.reshape((-1, 1, 2))
     cv2.fillPoly(phi, [airfoils], (255, 255, 255), lineType=cv2.LINE_AA)
     phi = cv2.flip(phi, 0)
-    phi = cv2.copyMakeBorder(phi, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=[-1, -1, -1])
+    phi = cv2.copyMakeBorder(phi, padding, padding, padding,
+                             padding, cv2.BORDER_CONSTANT, value=[-1, -1, -1])
     phi = rotating(phi, angle)
     phi = skfmm.distance(phi, dx=1, order=2)
 
@@ -78,3 +84,39 @@ def rendering_binary(name, angle, points, kind, re, ma):
     img = img.rotate(-1 * angle)
     img = img.resize((146, 146))
     img.save(f"out/{airfoil_image_name}")
+
+
+def rendering_spectro(name, angle, points, kind, re, ma):
+    airfoil_image_name = f"{name}_{kind}_{re}_{ma}_{angle}"
+    airfoil_image_name = airfoil_image_name.replace(" ", "")
+    px = points[:, 0]
+    py = points[:, 1]
+    fl = f"out/{airfoil_image_name}.jpg"
+    au = f"out/{airfoil_image_name}.wav"
+
+    matplotlib.use("Agg")
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(10, 10))
+    fig.tight_layout()
+    ax.plot(px, py)
+    ax.fill(px, py, "white", zorder=10)
+    ax.axes.axis("off")
+    ax.axes.set_aspect("equal", "datalim")
+    fig.savefig(fl, bbox_inches="tight", pad_inches=0)
+    plt.close(fig)
+
+    img = Image.open(fl)
+    img = img.rotate(-1 * angle)
+    img = img.resize((146, 146))
+    img.save(fl)
+
+    sg = SpectroGraphic(path=fl)
+    sg.save(wav_file=au)
+    Fs, aud = wavfile.read(au)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.specgram(aud, Fs=Fs, cmap=plt.get_cmap("viridis"))
+    ax.axes.axis("off")
+    fig.savefig(fl, bbox_inches="tight", pad_inches=0)
+    plt.close(fig)
+
+    os.remove(au)
