6ff9f5f1925d0d4a404b464bb84b5765bf12d454
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-28 19:45:32 +0700

    Refactor the arch cnn

--- src/services/arch/conv2.py
+++ src/services/arch/conv2.py
@@ -22,11 +22,11 @@ class Aerofoil2Relu1FC(AerofoilBaseNN):
         self.fc1 = nn.Linear(2000, 3)
 
     def forward(self, x):
-        f1 = self.conv1(x)
-        f2 = self.conv2(f1)
-        f3_flat = f2.view(f2.size(0), -1)
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out.view(out.size(0), -1)
 
-        return self.fc1(f3_flat)
+        return self.fc1(out)
 
 
 class Aerofoil2BN1FC(AerofoilBaseNN):
@@ -50,42 +50,43 @@ class Aerofoil2BN1FC(AerofoilBaseNN):
         self.fc1 = nn.Linear(2000, 3)
 
     def forward(self, x):
-        f1 = self.conv1(x)
-        f2 = self.conv2(f1)
-        f3_flat = f2.view(f2.size(0), -1)
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out.view(out.size(0), -1)
 
-        return self.fc1(f3_flat)
+        return self.fc1(out)
 
 
 class Aerofoil2BN3FC(AerofoilBaseNN):
-    def __init__(self):
-        super(Aerofoil2BN1FC, self).__init__()
+    def __init__(self, num_channel=1):
+        super(Aerofoil2BN3FC, self).__init__()
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, 3, 1),
-            nn.BatchNorm2d(32),
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(num_channel, 17, 3),
+            nn.BatchNorm2d(17),
+            nn.ReLU(),
+            nn.MaxPool2d(4, 4)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 15),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
 
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, 3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
+        self.fc1 = nn.Sequential(
+            nn.Linear(64*7*7, 600),
+            nn.Dropout(0.25)
         )
-
-        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
-        self.drop = nn.Dropout2d(0.25)
-        self.fc2 = nn.Linear(in_features=600, out_features=120)
-        self.fc3 = nn.Linear(in_features=120, out_features=3)
+        self.fc2 = nn.Linear(600, 120)
+        self.fc3 = nn.Linear(120, 3)
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
+        out = self.conv1(x)
+        out = self.conv2(out)
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
-        out = self.drop(out)
         out = self.fc2(out)
         out = self.fc3(out)
 
