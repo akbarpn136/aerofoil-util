b73f9e6d28de0ad9a3019c47121b4562fbf95c38
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-30 02:42:33 +0700

    Add new cnn arch

--- src/services/arch/conv2.py
+++ src/services/arch/conv2.py
@@ -57,6 +57,47 @@ class Aerofoil2BN1FC(AerofoilBaseNN):
         return self.fc1(out)
 
 
+class Aerofoil2BN2FC(AerofoilBaseNN):
+    def __init__(self, num_channel=1):
+        super(Aerofoil2BN2FC, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(num_channel, 32, 15),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 15),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.4)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 15),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 15),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.4)
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(64*11*11, 128),
+            nn.ReLU(),
+            nn.Dropout(0.4)
+        )
+        self.fc2 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+
+        return out
+
+
 class Aerofoil2BN3FC(AerofoilBaseNN):
     def __init__(self, num_channel=1):
         super(Aerofoil2BN3FC, self).__init__()
