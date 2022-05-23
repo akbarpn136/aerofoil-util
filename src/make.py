699b2b0faa75d6749d6bc64b34cdcf3cd82fd1cf
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-05-23 09:13:21 +0700

    Update function to make random airfoil

--- src/make.py
+++ src/make.py
@@ -3,25 +3,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def airfoil():
-    points = np.array([
-        [1, 0.00],  # trailing edge (top)
-        [0.76, 0.08],
-        [0.52, 0.125],
-        [0.25, 0.12],
-        [0.1, 0.08],
-        [0, 0.03],  # leading edge (top)
-        [0, -0.03],  # leading edge (bottom)
-        [0.15, -0.08],
-        [0.37, -0.01],
-        [0.69, 0.04],
-        [1, -0.00],  # trailing edge (bottom)
-    ])
+def airfoil(num=100):
+    for i in range(num):
+        randpoints = np.array([
+            [1, 0.00],  # trailing edge (top)
+            [0.76, np.random.uniform(0.04, 0.1)],
+            [0.52, np.random.uniform(0.1, 0.2)],
+            [0.25, np.random.uniform(0.1, 0.2)],
+            [0.1, np.random.uniform(0, 0.1)],
+            [0, np.random.uniform(0, 0.06)],  # leading edge (top)
+            [0, -1 * np.random.uniform(0, 0.06)],  # leading edge (bottom)
+            [0.15, -1 * np.random.uniform(0, 0.1)],
+            [0.37, -1 * np.random.uniform(0, 0.1)],
+            [0.69, np.random.uniform(-0.04, 0.04)],
+            [1, 0.00],  # trailing edge (bottom)
+        ])
 
-    fig, ax = plt.subplots(figsize=(6, 6))
-    fig.tight_layout()
-    ax.axes.axis("off")
-    ax.axes.set_aspect("equal", "datalim")
-    curve = bezier.Curve(points.T, degree=points.shape[0] - 1)
-    curve.plot(100, ax=ax)
-    plt.show()
+        curve = bezier.Curve(randpoints.T, degree=randpoints.shape[0] - 1)
+        eval_range = np.linspace(0, 1, 100)
+        result = curve.evaluate_multi(eval_range)
+        np.savetxt(f"foil/aerofoil{i + 1}.dat", result.T, header=f"Aerofoil{i + 1}", comments="")
+
+
+if __name__ == "__main__":
+    airfoil()
