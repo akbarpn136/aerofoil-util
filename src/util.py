26ee376b284b8e14adf2cb4d6d80ac65efa48a8b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-05-23 09:21:09 +0700

    Create function to count aero data

---
+++ src/util.py
@@ -0,0 +1,18 @@
+import glob
+import pandas as pd
+
+
+def count_data():
+    path = "aero"
+    aeros = glob.glob(f"{path}/*.txt")
+
+    for aero in aeros:
+        ddf = pd.read_fwf(
+            aero,
+            skiprows=11,
+            header=None,
+            names=["alpha", "cl", "cd", "cdp", "cm", "TopXtr", "BotXtr", "Cpmin", "Chinge", "XCp"]
+        )
+
+        name = aero.replace("aero\\", "").replace("_T1_Re0.500_M0.00_N5.0.txt", "")
+        print(f"{name}: {ddf.alpha.count()}")
