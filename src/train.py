5009f37f4fb454f3c02752a599509f19aeb95579
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-08-15 19:31:48 +0700

    Fir comflicted file message

--- src/train.py
+++ src/train.py
@@ -12,11 +12,7 @@ from src.services.collection import AerofoilForceDataset
 if __name__ == '__main__':
     batch_size = 18
     num_channel = 3
-<<<<<<< Updated upstream
-    num_epochs = 300
-=======
     num_epochs = 1000
->>>>>>> Stashed changes
     learning_rate = 0.00001
 
     dataset = AerofoilForceDataset(
@@ -24,10 +20,7 @@ if __name__ == '__main__':
         "out",
         transform=transforms.Compose([
             transforms.Resize(78),
-<<<<<<< Updated upstream
-=======
             transforms.RandomPerspective(distortion_scale=0.6, p=1.0),
->>>>>>> Stashed changes
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) if num_channel == 3
             else transforms.Normalize((0.5,), (0.5,)),
@@ -37,16 +30,20 @@ if __name__ == '__main__':
     dataset_size = len(dataset)
     train_size = int(0.8 * dataset_size)
     valid_size = dataset_size - train_size
-    train_dataset, valid_dataset = random_split(dataset, (train_size, valid_size))
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=True)
+    train_dataset, valid_dataset = random_split(
+        dataset, (train_size, valid_size))
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_dataset,
+                              batch_size=batch_size, shuffle=True)
 
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = Aerofoil3BN2FC(num_channel=num_channel).to(dev)
     loss_func = nn.MSELoss()
     optim = torch.optim.Adam(model.parameters(), learning_rate)
 
-    model.fit(loss_func, optim, train_loader, valid_loader, device=dev, epochs=num_epochs)
+    model.fit(loss_func, optim, train_loader,
+              valid_loader, device=dev, epochs=num_epochs)
     torch.save(model.state_dict(), "aerofoil_stack_Aerofoil3BN2FC.pt")
     plt.plot(model.lossList, label="Train Loss")
     plt.plot(model.valid_lossList, label="Valid Loss")
